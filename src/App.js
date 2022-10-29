faa06c52aa1e00f9e534147d8461828922cab392
shorifuddin2 <shorifuddin240915@gmail.com>
2022-10-29 20:18:39 +0600

    user updete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState, useEffect} from 'react';
 
@@ -10,11 +10,39 @@ function App() {
     .then(res => res.json())
     .then(data => setUsers(data))
    }, [])
+
+   const handelSubmit = event =>{
+    event.preventDefault()
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name, email}
+
+    fetch('http://localhost:5000/users/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(user),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    const newUsers = [...users, data];
+    setUsers(newUsers);
+    console.log(data)
+  })
+
+   }
   return (
     <div className="App">
+
+  
      <h1>My Won Data</h1>
      <h1>Data: {users.length}</h1>
-
+     <form onSubmit={handelSubmit}>
+      <input type="name" name="name" placeholder='Name' required/>
+      <input type="email" name="email" placeholder='Email' required/>
+      <input type="submit" name="" value="Submit"/>
+     </form>
      <ul>
       {
         users.map(user =><li key={user.id}>{user.name}</li>)
