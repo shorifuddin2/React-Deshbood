1783dd422185c6a16f8341d76379c97883b4eeca
shorifuddin2 <shorifuddin240915@gmail.com>
2022-10-29 18:35:35 +0600

    user seen all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
 
 function App() {
+   const [users, setUsers] = useState([])
+
+   useEffect(() => {
+    fetch(`http://localhost:5000/users/`)
+    .then(res => res.json())
+    .then(data => setUsers(data))
+   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My Won Data</h1>
+     <h1>Data: {users.length}</h1>
+
+     <ul>
+      {
+        users.map(user =><li key={user.id}>{user.name}</li>)
+      }
+     </ul>
     </div>
   );
 }
